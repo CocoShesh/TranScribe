e7123162898101b1e145a5ff6745b919ca39a375
NARD <glenardrefamonte1@gmail.com>
2024-05-09 19:28:56 +0800

    Updated

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { UserProvider } from "./context/UserContext";
 import { ThemeProvider } from "./context/ThemeContext";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <UserProvider>
-      <ThemeProvider>
-        <App />
-      </ThemeProvider>
-    </UserProvider>
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
