6ae43132290aae68735549c6bcd0a243849f8891
NARD <glenardrefamonte1@gmail.com>
2024-05-10 00:48:51 +0800

    Updated

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,18 +1,15 @@
 import { useState } from "react";
+import "regenerator-runtime/runtime";
 import SpeechRecognition, {
   useSpeechRecognition,
 } from "react-speech-recognition";
-import "regenerator-runtime/runtime";
+
 import { FaMicrophoneAlt, FaArrowRight } from "react-icons/fa";
 const index = () => {
   const [isStartRecord, setIsStartRecord] = useState(false);
 
   const { transcript, browserSupportsSpeechRecognition } =
     useSpeechRecognition();
-
-  if (!browserSupportsSpeechRecognition) {
-    return <span>Browser doesn't support speech recognition.</span>;
-  }
 
   const startRecording = () => {
     SpeechRecognition.startListening({ continuous: true });
@@ -38,16 +35,6 @@ const index = () => {
         </section>
         {isStartRecord ? (
           <div
-            onClick={startRecording}
-            className="flex items-center w-[400px] cursor-pointer border hover:shadow hover:shadow-[#18315391] p-3 rounded-lg justify-between"
-          >
-            <h1 className="text-[#183153]  dark:text-[#0079ea] font-semibold text-xl">
-              Record
-            </h1>
-            <FaMicrophoneAlt />
-          </div>
-        ) : (
-          <div
             onClick={stopRecording}
             className="flex items-center w-[400px] cursor-pointer border hover:shadow hover:shadow-[#18315391] p-3 rounded-lg justify-between"
           >
@@ -56,8 +43,24 @@ const index = () => {
             </h1>
             <FaMicrophoneAlt />
           </div>
+        ) : (
+          <div
+            onClick={startRecording}
+            className="flex items-center w-[400px] cursor-pointer border hover:shadow hover:shadow-[#18315391] p-3 rounded-lg justify-between"
+          >
+            <h1 className="text-[#183153]  dark:text-[#0079ea] font-semibold text-xl">
+              Record
+            </h1>
+            <FaMicrophoneAlt />
+          </div>
         )}
-        <p>{transcript}</p>
+        {!browserSupportsSpeechRecognition && (
+          <div className="w-[400px] mt-5 text-center">
+            Browser doesn't support speech recognition. Sorry for the
+            inconvenience. Soon I will implement cross-browser support, but for
+            now, try another browser.
+          </div>
+        )}
       </section>
     </>
   );
