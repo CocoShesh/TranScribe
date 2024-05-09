0a012db75a716f0e8afd725920ba804c3703875f
NARD <glenardrefamonte1@gmail.com>
2024-05-09 19:43:48 +0800

    Updated

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import MainContent from "./components/Main";
 function App() {
   return (
     <>
-      <section className=" flex flex-col items-center min-h-screen   justify-center   w-full">
+      <section className=" flex flex-col items-center min-h-screen dark:bg-[#131313]  transform transition-colors duration-300 ease-in-out justify-center   w-full">
         <Header />
         <MainContent />
       </section>
