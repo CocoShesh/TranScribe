5326a9983b93683cd16632ebd74f424a3665d2ca
NARD <glenardrefamonte1@gmail.com>
2024-05-09 19:27:12 +0800

    Updated

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,8 @@
-const Header = () => {
+import React from "react";
+import { useTheme } from "../../context/ThemeContext";
+
+const Header: React.FC = () => {
+  const { handleChangeTheme, theme } = useTheme();
   return (
     <header className="flex items-center justify-between px-5 w-full  2xl:w-[1500px]">
       <div className="flex items-center gap-5">
@@ -8,7 +12,12 @@ const Header = () => {
         </h1>
       </div>
       <section className="h-7 w-12 relative rounded-full bg-[#0079ea]">
-        <div className="absolute h-5 w-5 bg-white rounded-full top-1 left-1"></div>
+        <div
+          onClick={handleChangeTheme}
+          className={`absolute h-5 w-5 cursor-pointer transition-transform duration-300 ease-in-out  bg-white rounded-full top-1  ${
+            theme ? "translate-x-6" : "translate-x-1"
+          }`}
+        ></div>
       </section>
     </header>
   );
