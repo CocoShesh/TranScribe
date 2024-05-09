0f2faf710568cf0f48c3585242ca164bed6a6280
NARD <glenardrefamonte1@gmail.com>
2024-05-09 19:43:52 +0800

    Updated

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,9 +5,13 @@ const Header = () => {
   return (
     <header className="flex items-center justify-between px-5 w-full  2xl:w-[1500px]">
       <div className="flex items-center gap-5">
-        <img src="/logo.png" alt="Logo" className="h-20 w-20" />
-        <h1 className="text-[#183153] font-bold text-3xl">
-          Tran<span className="text-[#0079ea]">Scribe</span>
+        <img
+          src={theme ? "/logo3.svg" : "/logo.png"}
+          alt="Logo"
+          className="h-20 w-20"
+        />
+        <h1 className="text-[#183153] dark:text-white font-bold text-3xl">
+          Tran<span className="text-[#0079ea] ">Scribe</span>
         </h1>
       </div>
       <section className="h-7 w-12 relative rounded-full bg-[#0079ea]">
