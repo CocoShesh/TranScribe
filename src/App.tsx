e9f6979c5da18770c07139e6a7c88f81f4687f2e
NARD <glenardrefamonte1@gmail.com>
2024-05-09 19:28:53 +0800

    Updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
-// import Card from "./components/Card";
-// import { User } from "./context/User";
-
 import Header from "./components/Header";
 import MainContent from "./components/Main";
 
 function App() {
   return (
     <>
-      {/* <User /> */}
-      {/* <Card /> */}
       <section className=" flex flex-col items-center min-h-screen   justify-center   w-full">
         <Header />
         <MainContent />
