058978229a922a941c1516cc15aea201c3261d25
NARD <glenardrefamonte1@gmail.com>
2024-05-10 18:05:14 +0800

    Updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,13 @@
 import Header from "./components/Header";
 import Button from "./components/Button";
-import Transcript from "./pages/Transcript";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <section className=" flex flex-col items-center min-h-screen dark:bg-[#131313]  transform transition-colors duration-300 ease-in-out justify-center   w-full">
-          <Header />
-          <Routes>
-            <Route path="/" element={<Button />} />
-            <Route path="/transcript" element={<Transcript />} />
-          </Routes>
-        </section>
-      </BrowserRouter>
+      <section className=" flex flex-col items-center min-h-screen dark:bg-[#131313]  transform transition-colors duration-300 ease-in-out justify-center   w-full">
+        <Header />
+        <Button />
+      </section>
     </>
   );
 }
