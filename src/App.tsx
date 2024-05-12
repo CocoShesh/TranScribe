d3ebe19847fd098d36f2bc07ed4848a84b183a70
NARD <glenardrefamonte1@gmail.com>
2024-05-13 00:04:51 +0800

    Updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import Header from "./components/Header";
 import Button from "./components/Button";
+
 function App() {
   return (
     <>
-      <section className=" flex flex-col items-center min-h-screen dark:bg-[#131313]  transform transition-colors duration-300 ease-in-out justify-center   w-full">
+      <section className=" flex flex-col items-center min-h-screen dark:bg-[#131313]  pb-10 transform transition-colors duration-300 ease-in-out justify-center   w-full">
         <Header />
         <Button />
       </section>
