f62281b3f3432c1455e127c3618ad638c41c7390
NARD <glenardrefamonte1@gmail.com>
2024-05-12 18:20:28 +0800

    Updated

---
+++ src/utils/WordUtils.ts
@@ -0,0 +1,10 @@
+export const countWords = (count: string) => {
+  if (count.length === 0) {
+    return 0;
+  } else {
+    count = count.replace(/(^\s*)|(\s*$)/gi, "");
+    count = count.replace(/[ ]{2,}/gi, " ");
+    count = count.replace(/\n /, "\n");
+    return count.split(" ").length;
+  }
+};
